31f0b461db3865e0f7660f2358ca216832ae33f1
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-05-13 15:32:53 -0500

    Time: 59 ms (13.19%), Space: 16.9 MB (64.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #return false if the strings are not the same size
+        if len(s) != len(t):
+            return False
+        #create dictionary to store values
+        letterFrequency = dict()
+        #initialize values in dictionary with 0 
+        for l in s:
+            letterFrequency[l] = 0
+        #populate dictionary with letters in s as keys
+        #and increment value by 1 
+        for l in s:
+            letterFrequency[l] += 1
+        #check if the the letters in t are in the populated dictionary
+        #decrement by 1
+        for l in t:
+            letterFrequency[l] = letterFrequency.get(l, 0) - 1
+            #If the letter frequency is less than 0 then return False
+            if letterFrequency[l] < 0:
+                return False
+
+        #Everything has passed the tests
+        return True
+        
