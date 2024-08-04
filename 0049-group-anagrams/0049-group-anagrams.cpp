20284f88ac852a75ad50f104fab9e41a46815538
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-08-04 14:01:17 -0500

    Time: 45 ms (15.14%), Space: 31.7 MB (11.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,36 +1,19 @@
-#include <unordered_map>
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //create a map that holds solutions and vector to store ascii values
-        map <vector<int>, vector<string>> map1;
+        map<vector<int>,vector<string>> mapr;
         vector<int> count (26,0);
-        vector<string> hold;
-        vector<vector<string>> result;
-
-        //iterate through strs getting individual strings
-        for(string s : strs){
+        vector<vector<string>> res;
+        
+        for(string str: strs){
             fill(count.begin(), count.end(), 0);
-            hold.push_back(s);
-            //loop through string to get characters present
-            for(char c: s){
-                count[(int)c - (int)'a'] += 1;
+            for(char chr: str){
+                count[(int)chr - (int)'a'] += 1;
             }
-            //populate map
-            if(map1.find(count) != map1.end()){
-                map1[count].push_back(s);  
-            }
-            else{
-              map1[count] =  hold; 
-            }
-            hold.clear();
+            mapr[count].push_back(str);
         }
-
-        for(auto x : map1){
-            result.push_back(x.second);
-        }
-
-        return result;
+        for(auto pair: mapr)
+            res.push_back(pair.second);
+        return res;
     }
 };
