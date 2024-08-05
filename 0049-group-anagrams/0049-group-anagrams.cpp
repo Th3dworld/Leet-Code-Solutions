dadc6109da6c64764ef16dfeedeb41841408ea3c
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-08-04 20:58:54 -0500

    Time: 38 ms (21.30%), Space: 31.7 MB (11.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>,vector<string>> mapr;
-        vector<int> count (26,0);
+        //Create variables
+        map<vector<int>, vector<string>> mapr;
+        vector <int> holdr(26,0);
         vector<vector<string>> res;
         
         for(string str: strs){
-            fill(count.begin(), count.end(), 0);
+            fill(holdr.begin(), holdr.end(), 0);
             for(char chr: str){
-                count[(int)chr - (int)'a'] += 1;
+                holdr[(int)chr - (int)'a'] += 1;
             }
-            mapr[count].push_back(str);
+            mapr[holdr].push_back(str);
         }
+        
         for(auto pair: mapr)
             res.push_back(pair.second);
         return res;
     }
 };
+
+
