979730f750cb1595ad2bf32ac78fcc0c1cfe8d58
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-08-06 14:59:14 -0500

    Time: 7 ms (84.94%), Space: 14.3 MB (24.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //Create map to map value to index
-        unordered_map<int,int>valueToIndex;
+        //Create variable that will map values to index
+        unordered_map<int,int> valueToIndex;
         
-        //iterate through vector adding values and index to map
-        //and checking if values that add to target exist
+        //iterate through vector populating map and checking if value that adds up to target exists
         for(int i = 0; i < nums.size(); i++){
             if(valueToIndex.contains(target - nums[i])){
                 return {i, valueToIndex[target - nums[i]]};
             }
             valueToIndex[nums[i]] = i;
         }
+        
         return {};
-    } 
+    }
 };
