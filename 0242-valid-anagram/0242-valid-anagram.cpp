4ea87404473ac784bf242cbfa89ef2e16be9b0d4
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-08-06 14:52:23 -0500

    Time: 3 ms (97.17%), Space: 10.1 MB (8.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //Check if strings are of equal length 
-        if(s.length() != t.length()){
+        //Check if strings are of equal length
+        if(s.size() != t.size())
             return false;
+        
+        //Create variables used to to store values
+        unordered_map<char, int> letterToOccurence;
+        
+        for(char chr: s){
+            letterToOccurence[chr] += 1;
         }
         
-        //create map to store letter to number of times it occurs
-        unordered_map<char, int> letterOccurence;
-        
-        for(char c: s){
-            letterOccurence[c] += 1;
-        }
-        
-        for(char c: t){
-            letterOccurence[c] -= 1;
-            if(letterOccurence[c] < 0){
+        for(char chr: t){
+            letterToOccurence[chr] -= 1;
+            if(letterToOccurence[chr] < 0)
                 return false;
-            }
         }
         
         return true;
