245745049ae3e254fbbd2e8deaf5f2dd3a68490b
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-09-20 01:32:33 -0500

    Time: 45 ms (75.84%), Space: 17 MB (27.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        #Check if lengths are equal
-        if len(s)  != len(t):
+        if len(s) != len(t):
             return False
         
-        #create dict to map letter to number of occurrences
-        letter_to_occur = dict()
+        hashMap = dict()
+        
         for char in s:
-            letter_to_occur[char] = letter_to_occur.get(char,0) + 1
+            hashMap[char] =  hashMap.get(char, 0) + 1
+        
         for char in t:
-            letter_to_occur[char] = letter_to_occur.get(char,0) - 1
-            if letter_to_occur[char] < 0: 
+            hashMap[char] =  hashMap.get(char, 0) - 1
+            if hashMap[char] < 0:
                 return False
+        
         return True
