7fa689a7a5432946c8582d8e32f04b6849caa629
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-09-20 02:07:38 -0500

    Time: 97 ms (28.57%), Space: 20.6 MB (43.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #create dictionary to store words as list
-        res = collections.defaultdict(list)
+        hashMap = defaultdict(list)
         
-        #create array to store occurrence of letters
         for string in strs:
-            count = [0] * 26            
-            for char in string:
-                count[ord(char) - ord('a')] += 1
-            res[tuple(count)].append(string)
-        return res.values()
+            key = ""
+            for char in sorted(string):
+                key += str(ord(char))
+            hashMap[key].append(string)
+        
+        return hashMap.values()
         
