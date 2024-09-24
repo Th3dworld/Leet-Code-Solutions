f10029503a7ad0072abc5f2d44173d6ee2d1c853
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-09-23 23:34:15 -0500

    Time: 45 ms (75.54%), Space: 16.9 MB (91.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(t) != len(s):
             return False
         
-        hashMap = dict()
+        occur_s = {}
+        occur_t = {}
         
-        for char in s:
-            hashMap[char] =  hashMap.get(char, 0) + 1
+        for i in range(len(s)):
+            occur_s[s[i]] = occur_s.get(s[i],0) + 1
+            occur_t[t[i]] = occur_t.get(t[i],0) + 1
+            
+        return (occur_s == occur_t)
         
-        for char in t:
-            hashMap[char] =  hashMap.get(char, 0) - 1
-            if hashMap[char] < 0:
-                return False
-        
-        return True
