e7c0ed714547c4143965a5869f875fe487ccd236
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-10-10 03:39:45 -0500

    Time: 44 ms (79.40%), Space: 17 MB (55.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(t) != len(s):
+        if len(s) != len(t):
             return False
-        
-        occur_s = {}
-        occur_t = {}
-        
-        for i in range(len(s)):
-            occur_s[s[i]] = occur_s.get(s[i],0) + 1
-            occur_t[t[i]] = occur_t.get(t[i],0) + 1
-            
-        return (occur_s == occur_t)
+        mapS = Counter(s)
+        mapT = Counter(t)
+        return mapS == mapT
         
