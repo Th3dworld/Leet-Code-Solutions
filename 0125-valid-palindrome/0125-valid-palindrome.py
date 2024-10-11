2944f661a65c48d0d4aa9442785cd03faf88d126
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-10-11 01:00:38 -0500

    Time: 38 ms (89.04%), Space: 17.2 MB (45.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        res = ""
-        
+        string = ""
         for c in s:
             if c.isalpha() or c.isdigit():
-                res += c.lower()
-        return res == res[::-1]
+                string += c.lower()
+        return string == string[::-1]
