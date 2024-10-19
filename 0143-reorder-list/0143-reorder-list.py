c7038fad2685ab0574d8ec1a090c21fe9c8d1a88
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-10-19 14:53:23 -0500

    Time: 0 ms (100.00%), Space: 22.4 MB (99.47%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -11,20 +11,20 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
             
-        
         second = slow.next
         prev = slow.next = None
         
         while second:
-            nxt = second.next
+            tmp = second.next
             second.next = prev
             prev = second
-            second = nxt
-        
+            second = tmp
+            
         first,second = head,prev
+        
         while second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
             first,second = tmp1,tmp2
-            
+        
