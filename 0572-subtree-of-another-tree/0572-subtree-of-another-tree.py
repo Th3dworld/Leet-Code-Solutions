9bc38836e328af3475eac66e24acaaa4859142f8
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-10-26 01:49:44 -0500

    Time: 40 ms (96.69%), Space: 16.8 MB (69.13%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -9,34 +9,18 @@ class Solution:
         if not s: return True
         if not t: return False
         
-        if self.sameTree(t,s):
+        if self.isSameTree(t,s):
             return True
         
-        return (self.isSubtree(t.left,s) or self.isSubtree(t.right,s))
-    
-    def sameTree(self,p, q):
-        if not p and not q:
-            return True           
-        if  p and q and p.val == q.val:
-            return (self.sameTree(p.left, q.left) and self.sameTree(p.right, q.right))
+        return self.isSubtree(t.left,s) or self.isSubtree(t.right,s)
+        
+        
+
+    def isSameTree(self,t,s):
+        if not t and not s:
+            return True
+        if t and s and t.val == s.val:
+            return self.isSameTree(t.left, s.left) and self.isSameTree(t.right, s.right) 
         else:
             return False
-    
-#     class Solution:
-#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-#         if not subRoot:
-#             return True
-#         if not root:
-#             return False
 
-#         if self.isSameTree(root, subRoot):
-#             return True
-#         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-
-#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-#         if not p and not q:
-#             return True
-#         if p and q and p.val == q.val:
-#             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-#         else:
-#             return False
