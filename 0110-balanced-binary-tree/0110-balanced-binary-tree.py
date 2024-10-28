f77a9128c735c9e5b42371f61f085249aeea69a7
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-10-27 19:55:12 -0500

    Time: 3 ms (69.01%), Space: 18.3 MB (6.35%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -10,8 +10,9 @@ class Solution:
         def dfs(curr):
             if not curr: return [True, 0]
             
-            left = dfs(curr.left)
             right = dfs(curr.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [balanced, 1 + max(right[1], left[1])]
+            left = dfs(curr.left)
+            balanced = right[0] and left[0] and abs(left[1] - right[1]) <= 1
+            
+            return [balanced, 1 + max(left[1],right[1])]
         return dfs(root)[0]
