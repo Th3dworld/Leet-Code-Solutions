5d5fb8fcaa55a38cc782de35f135c5ea8f9912dd
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-10-29 23:40:48 -0500

    Time: 0 ms (100.00%), Space: 18 MB (14.25%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -7,13 +7,12 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         q = deque()
-        
+        depth = 0
         if root:
             q.append(root)
-            
-        level = 0
         
-        while q:        
+        
+        while q:     
             for i in range(len(q)):
                 node = q.popleft()
                 
@@ -21,6 +20,5 @@ class Solution:
                     q.append(node.right)
                 if node.left:
                     q.append(node.left)
-            level += 1
-        return level
-        
+            depth += 1
+        return depth
