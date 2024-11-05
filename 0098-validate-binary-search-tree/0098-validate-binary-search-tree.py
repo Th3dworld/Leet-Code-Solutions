7875aadebd035225942943191d6d4b6e883b319b
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-04 21:51:54 -0600

    Time: 3 ms (35.43%), Space: 18.8 MB (19.66%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -11,5 +11,5 @@ class Solution:
                 return True
             if not (left < node.val < right):
                 return False
-            return valid(node.left,left, node.val) and valid(node.right,node.val, right) 
+            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
         return valid(root, float("-inf"), float("inf"))
