10887c846d6b03d58d2eca6e38eced9a73e9b439
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-04 21:25:16 -0600

    Time: 56 ms (48.34%), Space: 20.3 MB (52.82%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,7 +8,6 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         curr = root
-        
         while curr:
             if curr.val < p.val and curr.val < q.val:
                 curr = curr.right
@@ -16,3 +15,4 @@ class Solution:
                 curr = curr.left
             else:
                 return curr
+            
