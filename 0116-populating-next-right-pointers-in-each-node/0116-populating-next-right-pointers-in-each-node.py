a7eb056573ffa6b7ee2c3b96f216cff04af1ff78
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-10 14:11:17 -0600

    Time: 51 ms (39.52%), Space: 18.3 MB (5.48%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -14,17 +14,22 @@ class Solution:
             return root
         
         q = deque([root])
+        
         while q:
-            count = len(q)
+            length = len(q)
             for i in range(len(q)):
                 node = q.popleft()
-                count -=1
-                if count == 0:
+                length -= 1
+                if node and length == 0:
                     node.next = None
-                else:
+                elif node and q:
                     node.next = q[0]
+                
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
         return root
+                    
+                    
+                    
