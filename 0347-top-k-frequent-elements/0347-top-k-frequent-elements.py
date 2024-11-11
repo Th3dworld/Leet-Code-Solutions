18cd2d628cd58b545692e498f251218fe0cf7dd1
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-10 19:07:48 -0600

    Time: 7 ms (54.98%), Space: 23.2 MB (7.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        Map = Counter(nums)
         bucket = [[] for i in range(len(nums) + 1)]
+        count = Counter(nums)
         
-        for key, v in Map.items():
-            bucket[v].append(key)
+        for v,c in count.items():
+            bucket[c].append(v)
             
         res = []
         
-        for i in range(len(nums),-1,-1):
-                res += bucket[i]
+        for i in range(len(nums),0,-1):
+            for j in bucket[i]:
+                res.append(j)
                 if len(res) == k:
                     return res
+        
+        
         
