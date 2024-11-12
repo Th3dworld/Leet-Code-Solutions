df1fb12d4be620e7c3e2d45ebe9de17135840dc0
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-12 15:38:57 -0600

    Time: 439 ms (91.77%), Space: 19.9 MB (68.54%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,6 +7,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
+                
             if i > 0 and nums[i] == nums[i-1]:
                 continue
                 
