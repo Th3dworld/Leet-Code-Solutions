a2bec89427a36f30a832d70ccd38b975b04f100d
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-12 15:14:24 -0600

    Time: 4147 ms (13.30%), Space: 33.4 MB (24.73%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,14 +3,13 @@ class Solution:
         hashSet = set(nums)
         longest = 0
         
-        for i in nums:
-            if i-1 not in hashSet:
+        for i in range(len(nums)):
+            if nums[i] - 1 not in hashSet:
                 length = 0
                 
-                while i + length in hashSet:
+                while nums[i] + length in hashSet:
                     length += 1
-                    
+                
                 longest = max(length, longest)
-            
         
         return longest
