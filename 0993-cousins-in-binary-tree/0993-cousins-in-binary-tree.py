e71f619f33fb44c263d4fd8aa3befd97c7cf1b15
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-13 00:46:43 -0600

    Time: 0 ms (100.00%), Space: 16.7 MB (46.57%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -7,21 +7,23 @@
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         q = deque([root])
+        hashMap = {}
         
         while q:
-            hashMap = {}
+            hashSet = set()
             for i in range(len(q)):
                 node = q.popleft()
+                hashSet.add(node.val)
                 
                 if node.right:
                     q.append(node.right)
-                    hashMap[node.right.val] = node.val
-                    
+                    hashMap[node.right.val] = node
+                
                 if node.left:
                     q.append(node.left)
-                    hashMap[node.left.val] = node.val
-            
-            if x in hashMap and y in hashMap and hashMap[x] != hashMap[y]:
-                    return True
+                    hashMap[node.left.val] = node
                 
+            if x in hashSet and y in hashSet and hashMap[x] != hashMap[y]:
+                return True
+        
         return False
