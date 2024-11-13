2503f11b14b914e6a91bd8595306f87876d68870
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-12 22:26:33 -0600

    Time: 3 ms (79.18%), Space: 20.7 MB (8.05%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,33 +6,29 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        if not root.left and not root.right:
-            return [root.val]
+        count = {}
         
-        q = deque([root])
-        hashMap = {}
-        res = []
-        maxi = 0
-        
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.val in hashMap:
-                    hashMap[node.val] += 1
-                    maxi = max(maxi, hashMap[node.val])
-                else:
-                    hashMap[node.val] = 1
-                    maxi = max(maxi, hashMap[node.val])
-                if node.right:
-                    q.append(node.right)
-                if node.left:
-                    q.append(node.left)
-        print(hashMap)
-        for k,v in hashMap.items():
-            if v == maxi:
-                res.append(k)
+        def dfs(root):
+            if not root:
+                return
             
+            if root.val in count:
+                count[root.val] += 1
+            else:
+                count[root.val] = 1
+            
+            dfs(root.left)
+            dfs(root.right)
+        
+        dfs(root)
+        res = []
+        mode = 0
+        
+        for k,v in count.items():
+            mode = max(mode, v)
+        
+        for k,v in count.items():
+            if v == mode:
+                res.append(k)
+        
         return res
-        
-        
-        
