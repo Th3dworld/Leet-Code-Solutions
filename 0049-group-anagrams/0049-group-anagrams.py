e8ef7e4976bbbb6fe1452dab40b36cc388293a5a
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-11-15 10:50:29 -0600

    Time: 23 ms (20.02%), Space: 21.8 MB (21.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group = collections.defaultdict(list)
+        hashMap = defaultdict(list)
         
         for string in strs:
-            key = [0] * 26 
+            key = [0] * 26
             
             for char in string:
                 key[ord(char) - ord('a')] += 1
-                
-            group[tuple(key)].append(string)
+            
+            hashMap[tuple(key)].append(string)
         
-        return list(group.values())
-        
+        return list(hashMap.values())
