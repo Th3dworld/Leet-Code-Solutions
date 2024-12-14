3133e0ec88eb531c306179a2ce708ac7a87c818c
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-12-14 15:14:47 -0600

    Time: 27 ms (48.75%), Space: 22.8 MB (48.30%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,11 +5,11 @@ class Solution:
         post = 1
         
         for i in range(len(nums)):
-            res[i] = pre
+            res[i] *= pre
             pre *= nums[i]
-        
-        for i in range(len(nums)-1, -1, -1):
+            
+        for i in range(len(nums)-1,-1,-1):
             res[i] *= post
             post *= nums[i]
-            
+        
         return res
