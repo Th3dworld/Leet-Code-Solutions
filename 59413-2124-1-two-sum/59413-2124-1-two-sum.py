dca8916c776472a03097d9321c6c32ee2a8149ac
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2024-12-27 20:18:59 -0600

    Time: 0 ms, Memory: 18.9 MB - LeetHub

---
+++ 59413-2124-1-two-sum/59413-2124-1-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+        
+        for i,v in enumerate(nums):
+            if target - v in prevMap:
+                return [prevMap[target -v], i]
+            prevMap[v] = i
+        
+        return []
