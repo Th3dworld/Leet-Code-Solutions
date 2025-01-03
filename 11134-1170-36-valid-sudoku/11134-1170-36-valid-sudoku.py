8826539c09b9fe72d6d9ab5f898e475ff81133cc
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2025-01-03 07:18:43 -0600

    Time: 3 ms, Memory: 18 MB - LeetHub

---
+++ 11134-1170-36-valid-sudoku/11134-1170-36-valid-sudoku.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        ROWS = collections.defaultdict(set)
+        COLS = collections.defaultdict(set)
+        GRID = collections.defaultdict(set)
+        
+        for row in range(len(board)):
+            for col in range(len(board)):
+                if board[row][col] == ".":
+                    continue
+                elif board[row][col] in ROWS[row]:
+                    return False
+                elif board[row][col] in COLS[col]:
+                    return False
+                elif board[row][col] in GRID[(row//3, col//3)]:
+                    return False
+                else:
+                    GRID[(row//3, col//3)].add(board[row][col])
+                    COLS[col].add(board[row][col])
+                    ROWS[row].add(board[row][col])
+        
+        return True
+                
