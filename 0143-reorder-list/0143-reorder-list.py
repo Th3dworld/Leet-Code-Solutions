ecbad147bd5b6cca4a057c15863b345212f2d747
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2025-03-26 01:08:06 -0500

    Time: 0 ms (100%), Space: 23.5 MB (12.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,27 +8,32 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        fast, slow = head, head
-        
+        slow, fast = head,head.next
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
         cur = slow.next
         slow.next = prev = None
-        
+
         while cur:
             tmp = cur.next
             cur.next = prev
             prev = cur
             cur = tmp
         
-        first = head
-        second = prev
-        
-        while second:
-            tmp1, tmp2 = first.next, second.next
+        first, second = head, prev
+
+        while first and second:
+            tmp1 = first.next
+            tmp2 = second.next
             first.next = second
             second.next = tmp1
-            first = tmp1
-            second = tmp2
+            first, second = tmp1,tmp2
+        
+        return head
+
+
+
+    
