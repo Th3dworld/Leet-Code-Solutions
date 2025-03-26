960af55473e71ff37ebd1835643bfcaf24510985
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2025-03-26 03:36:33 -0500

    Time: 0 ms (100%), Space: 18.1 MB (71.64%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,52 +1,19 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROW, COL = len(matrix),len(matrix[0])
+        if matrix[0][0] > target:
+            return False
         
-        top,bot = 0,ROW-1
-        while top <= bot:
-            row = (top+bot)//2
-            if matrix[row][-1] < target:
-                top = row + 1
-            elif matrix[row][0] > target:
-                bot = row - 1
-            else:
-                break
+        for i in range(len(matrix)):
+            if matrix[i][0] <= target and matrix[i][-1] >= target:
+                l,r = 0, len(matrix[i]) - 1
+
+                while l <= r:
+                    m = (l + r)//2
+                    if matrix[i][m] > target:
+                        r = m - 1
+                    elif matrix[i][m] < target:
+                        l = m + 1
+                    else:
+                        return True
         
-        if not (top<=bot): return False
-        row = (top+bot)//2
-        l,r = 0,COL-1
-        while l <= r:
-            m = (l+r)//2
-            if matrix[row][m] > target:
-                r = m-1
-            elif matrix[row][m] < target:
-                l = m+1
-            else:
-                return True
         return False
-        
-#         ROWS, COLS = len(matrix), len(matrix[0])
-
-#         top, bot = 0, ROWS - 1
-#         while top <= bot:
-#             row = (top + bot) // 2
-#             if target > matrix[row][-1]:
-#                 top = row + 1
-#             elif target < matrix[row][0]:
-#                 bot = row - 1
-#             else:
-#                 break
-
-#         if not (top <= bot):
-#             return False
-#         row = (top + bot) // 2
-#         l, r = 0, COLS - 1
-#         while l <= r:
-#             m = (l + r) // 2
-#             if target > matrix[row][m]:
-#                 l = m + 1
-#             elif target < matrix[row][m]:
-#                 r = m - 1
-#             else:
-#                 return True
-#         return False
