9f7ef32b6046550e385b55c4376a5379634fb914
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2025-04-09 22:08:29 -0500

    Time: 3 ms (64.68%), Space: 19.2 MB (17.33%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -7,15 +7,15 @@ class Solution:
                 case "+":
                     second, first = stack.pop(), stack.pop()
                     stack.append(first + second)
-                case "*":
-                    second, first = stack.pop(), stack.pop()
-                    stack.append(first * second)
                 case "-":
                     second, first = stack.pop(), stack.pop()
                     stack.append(first - second)
+                case "*":
+                    second, first = stack.pop(), stack.pop()
+                    stack.append(first * second)
                 case "/":
                     second, first = stack.pop(), stack.pop()
-                    stack.append(int(first/second))
+                    stack.append(int(first / second))
                 case _:
                     stack.append(int(token))
         
