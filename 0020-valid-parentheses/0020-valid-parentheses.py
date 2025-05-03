a21480e9c306e96504e4b20164babcdb7b3e1cf5
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2025-05-02 22:07:08 -0500

    Time: 0 ms (100%), Space: 17.7 MB (96.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        hashMap = {"]":"[", "}":"{", ")":"("}
+        closedToOpen = {"]":"[","}":"{",")":"("}
         stack = []
 
         for c in s:
-            if c in hashMap:
-                if stack and hashMap[c] == stack[-1]:
+            if c in closedToOpen:
+                if stack and stack[-1] == closedToOpen[c]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
 
-        
         return not stack
+        
