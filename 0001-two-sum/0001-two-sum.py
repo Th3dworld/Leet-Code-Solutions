e3ae1ec1a4a3579008ee714a7b7b933e6bfd911d
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2025-06-09 06:23:31 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         valueToIndex = {}
 
-        for i,v in enumerate(nums):
-            if target - v in valueToIndex:
-                return [valueToIndex[target-v], i]
-            valueToIndex[v] = i
+        for val, ind in enumerate(nums):
+            if target - val in valueToIndex:
+                return [valueToIndex[target - val], ind]
+            valueToIndex[val] = ind
         
         return []
