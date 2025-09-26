54c3bf0b1ff7ef19a8242242c2a9c3ef4ac47c87
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2025-09-25 23:32:52 -0500

    Time: 0 ms (100%), Space: 18.1 MB (66.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,22 +1,43 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l,r = 0, len(nums)-1
-        
-        while l<=r:
-            m = (l + r)//2
+        l,r = 0, len(nums) - 1
+
+
+        while l <= r:
+            m = l + (r - l)//2
             if nums[m] == target:
                 return m
-            
-            if nums[l] <= nums[m]:
-                if target > nums[m] or target < nums[l]:
-                    l = m + 1
-                else:
-                    r = m - 1
-            
-            else:
-                if target < nums[m] or target > nums[r]:
+            elif nums[l] >= nums[m]:
+                if nums[m] < target or nums[r] < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
+            elif nums[l] < nums[m]:
+                if nums[m] > target or nums[r] > nums[m]:
+                    l = m + 1
+                else:
+                    r = m - 1
         
+        return -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
         return -1
