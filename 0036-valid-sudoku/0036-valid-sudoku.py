40da284d9db64b679a1886c5285b72f8c4f9e790
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2026-01-15 20:19:48 -0600

    Time: 3 ms (81.22%), Space: 19.3 MB (13.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -5,14 +5,13 @@ class Solution:
         GRID = collections.defaultdict(set)
 
         for row in range(len(board)):
-            for col in range(len(board[0])):
+            for col in range(len(board)):
                 if board[row][col] == ".":
                     continue
-                elif board[row][col] in ROWS[row] or board[row][col] in COLS[col] or board[row][col] in GRID[(row//3, col//3)]:
+                if board[row][col] in ROWS[row] or board[row][col] in COLS[col] or board[row][col] in GRID[(row//3,col//3)]:
                     return False
                 else:
                     ROWS[row].add(board[row][col])
                     COLS[col].add(board[row][col])
-                    GRID[(row//3, col//3)].add(board[row][col])
-        
+                    GRID[(row//3,col//3)].add(board[row][col])
         return True
