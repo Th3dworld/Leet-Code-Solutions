d726457bf902134b5fb959fa3df9051133a8e863
Matanda Hillary Phiri <108048407+Th3dworld@users.noreply.github.com>
2026-01-15 19:54:07 -0600

    Time: 8 ms (34.3%), Space: 25 MB (5.63%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        countGrouped = [[] for i in range(len(nums)+1)]
+        countr = Counter(nums)
+        sortr = [[] for i in range(len(nums) + 1)]
         res = []
 
-        for key, value in count.items():
-            countGrouped[value].append(key)
+        for val, count in countr.items():
+            sortr[count].append(val)
 
-        for i in range(len(nums),-1,-1):
-            for n in countGrouped[i]:
+        for i in range(len(nums),0,-1):
+            for n in sortr[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
-        
-        return []
